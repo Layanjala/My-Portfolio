2c1db8b9ab3beb0c0c50ee235eac78c976c4e0db
Layanjala <layanjalajayasundara@gmail.com>
2025-11-19 18:32:47 +0530

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
-	basePath: "/My-Portfolio",
-	assetPrefix: "/My-Portfolio/",
 	images: {
 		unoptimized: true,
 	},
